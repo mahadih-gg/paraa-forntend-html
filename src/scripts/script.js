9d47972d3d5afff9d50a2fffdb5bba3ee1489478
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2024-01-21 20:01:03 +0600

    some issue fixed

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,4 @@
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
-
 const toggleNavbar = () => {
   var navbarLinks = document.querySelector(".navbar-links");
   navbarLinks.classList.toggle("nav_show");
 };
-
-// const swiper = new Swiper(...)
